5e9971a1fd66d2b16aea5415d339a2bd681b9aa3
Joshua C. Burt <jburt@healthwise.org>
2016-03-22 14:46:49 -0600

    based on unit tests very few methods need to be public

--- libraries/provider_jgit.rb
+++ libraries/provider_jgit.rb
@@ -74,6 +74,208 @@ class Chef
         end
       end
 
+      def target_revision
+        @target_revision ||= sha_hash?(@new_resource.revision) ? @new_resource.revision : remote_resolve_reference
+      end
+
+      alias revision_slug target_revision
+
+      def clone
+        converge_by("clone from #{@new_resource.repository} into #{cwd}") do
+          if @new_resource.uploadpack_allow_reachable_sha1_in_want && (git_minor_version >= Gem::Version.new('2.5.0'))
+            # Introduced in git 2.5 // uploadpack.allowReachableSHA1InWant
+            # https://github.com/git/git/blob/v2.5.0/Documentation/config.txt#L2570
+            clone_by_any_ref
+          else
+            clone_by_advertized_ref
+          end
+        end
+      end
+
+      def checkout
+        sha_ref = target_revision
+        converge_by("checkout ref #{sha_ref} branch #{@new_resource.revision}") do
+          # checkout into a local branch rather than a detached HEAD
+          run_opts = { cwd: cwd }
+          git_standard_executor(['branch', '-f', @new_resource.checkout_branch, sha_ref], run_opts)
+          git_standard_executor(['checkout', @new_resource.checkout_branch], run_opts)
+          Chef::Log.info "#{@new_resource} checked out branch: #{@new_resource.revision} onto: #{@new_resource.checkout_branch} reference: #{sha_ref}"
+        end
+      end
+
+      # Updated as per https://git-scm.com/book/en/v2/Git-Tools-Submodules
+      # Address https://github.com/chef/chef/issues/4126 (CHEF-4126)
+      def enable_submodules
+        if @new_resource.enable_submodules
+          converge_by("enable git submodules for #{@new_resource}") do
+            Chef::Log.info "#{@new_resource} synchronizing git submodules"
+            run_opts = { cwd: cwd }
+            git_standard_executor(['submodule', 'init'], run_opts)
+
+            Chef::Log.info "#{@new_resource} enabling git submodules"
+            # the --recursive flag means we require git 1.6.5+ now, see CHEF-1827
+            git_standard_executor(['submodule', 'update', '--init', '--recursive'], run_opts)
+          end
+        end
+      end
+
+      def fetch_updates
+        converge_by("fetch updates for #{@new_resource.remote}") do
+          # uploadpack.allowReachableSHA1InWant introduced in git 2.5.0
+          if @new_resource.uploadpack_allow_reachable_sha1_in_want && (git_minor_version >= Gem::Version.new('2.5.0'))
+            fetch_by_any_ref
+          else
+            setup_remote_tracking_branches(@new_resource.remote, @new_resource.repository)
+            fetch_by_advertized_ref
+          end
+        end
+      end
+
+      def find_current_revision
+        Chef::Log.info("#{@new_resource} finding current git revision")
+        if ::File.exist?(::File.join(cwd, '.git'))
+          # 128 is returned when we're not in a git repo. this is fine
+          run_opts = { cwd: cwd, returns: [0, 128] }
+          rev_parse_result = git_standard_executor(['rev-parse', 'HEAD'], run_opts)
+          rev = rev_parse_result.stdout.strip
+        end
+        sha_hash?(rev) ? rev : nil
+      end
+
+      def setup_remote_tracking_branches(remote_name, remote_url)
+        converge_by("set up remote tracking branches for #{remote_url} at #{remote_name}") do
+          Chef::Log.info "#{@new_resource} configuring remote tracking branches for repository #{remote_url} at remote #{remote_name}"
+          run_opts = { cwd: cwd, returns: [0, 1, 2] }
+          remote_status = git_standard_executor(['config', '--get', "remote.#{remote_name}.url"], run_opts)
+
+          run_opts = { cwd: cwd }
+          case remote_status.exitstatus
+            when 0, 2
+              # * Status 0 means that we already have a remote with this name, so we should update the url
+              #   if it doesn't match the url we want.
+              # * Status 2 means that we have multiple urls assigned to the same remote (not a good idea)
+              #   which we can fix by replacing them all with our target url (hence the --replace-all option)
+
+              if multiple_remotes?(remote_status) || !remote_matches?(remote_url, remote_status)
+                git_standard_executor(['config', '--replace-all', "remote.#{remote_name}.url", remote_url], run_opts)
+              end
+            when 1
+              git_standard_executor(['remote', 'add', remote_name, remote_url], run_opts)
+          end
+        end
+      end
+
+      def add_remotes
+        unless @new_resource.additional_remotes.empty?
+          @new_resource.additional_remotes.each_pair do |remote_name, remote_url|
+            converge_by("add remote #{remote_name} from #{remote_url}") do
+              Chef::Log.info "#{@new_resource} adding git remote #{remote_name} = #{remote_url}"
+              setup_remote_tracking_branches(remote_name, remote_url)
+            end
+          end
+        end
+      end
+
+      def multiple_remotes?(check_remote_command_result)
+        check_remote_command_result.exitstatus == 2
+      end
+
+      def remote_matches?(remote_url, check_remote_command_result)
+        check_remote_command_result.stdout.strip.eql?(remote_url)
+      end
+
+      private
+
+      def run_options(run_opts = {})
+        env = {}
+        if @new_resource.user
+          run_opts[:user] = @new_resource.user
+          # Certain versions of `git` misbehave if git configuration is
+          # inaccessible in $HOME. We need to ensure $HOME matches the
+          # user who is executing `git` not the user running Chef.
+          env['HOME'] = begin
+            require 'etc'
+            Etc.getpwnam(@new_resource.user).dir
+          rescue ArgumentError # user not found
+            raise Chef::Exceptions::User, "Could not determine HOME for specified user '#{@new_resource.user}' for resource '#{@new_resource.name}'"
+          end
+        end
+        run_opts[:group] = @new_resource.group if @new_resource.group
+        env['GIT_SSH'] = @new_resource.ssh_wrapper if @new_resource.ssh_wrapper
+        run_opts[:log_tag] = @new_resource.to_s
+        run_opts[:timeout] = @new_resource.timeout if @new_resource.timeout
+        env.merge!(@new_resource.environment) if @new_resource.environment
+        run_opts[:environment] = env unless env.empty?
+        run_opts
+      end
+
+      def cwd
+        @new_resource.destination
+      end
+
+      def git(*args)
+        ['git', *args].compact.join(' ')
+      end
+
+      def git_standard_executor(args, run_opts = {})
+        git_command = git(args)
+        Chef::Log.info "> #{git_command}"
+        shell_out!(git_command, run_options(run_opts))
+      end
+
+      def sha_hash?(string)
+        string =~ /^[0-9a-f]{40}$/
+      end
+
+      def refs_search(refs, pattern)
+        refs.find_all { |m| m[1] == pattern }
+      end
+
+      def git_ls_remote(rev_pattern)
+        stdout_obj = git_standard_executor ['ls-remote', "\"#{@new_resource.repository}\"", "\"#{rev_pattern}\""]
+        stdout_obj.stdout
+      end
+
+      def rev_search_pattern
+        if ['', 'HEAD'].include? @new_resource.revision
+          'HEAD'
+        else
+          @new_resource.revision + '*'
+        end
+      end
+
+      def rev_match_pattern(prefix, revision)
+        revision.start_with?(prefix) ? revision : prefix + revision
+      end
+
+      def find_revision(refs, revision, suffix = '')
+        found = refs_search(refs, rev_match_pattern('refs/tags/', revision) + suffix)
+        found = refs_search(refs, rev_match_pattern('refs/heads/', revision) + suffix) if found.empty?
+        found = refs_search(refs, revision + suffix) if found.empty?
+        found
+      end
+
+      def remote_resolve_reference
+        Chef::Log.info("#{@new_resource} resolving remote reference")
+        # The sha pointed to by an annotated tag is identified by the
+        # '^{}' suffix appended to the tag. In order to resolve
+        # annotated tags, we have to search for "revision*" and
+        # post-process. Special handling for 'HEAD' to ignore a tag
+        # named 'HEAD'.
+        @resolved_reference = git_ls_remote(rev_search_pattern)
+        refs = @resolved_reference.split("\n").map { |line| line.split("\t") }
+        # First try for ^{} indicating the commit pointed to by an
+        # annotated tag.
+        # It is possible for a user to create a tag named 'HEAD'.
+        # Using such a degenerate annotated tag would be very
+        # confusing. We avoid the issue by disallowing the use of
+        # annotated tags named 'HEAD'.
+
+        found = rev_search_pattern != 'HEAD' ? find_revision(refs, @new_resource.revision, '^{}') : refs_search(refs, 'HEAD')
+        found = find_revision(refs, @new_resource.revision) if found.empty?
+        found.size == 1 ? found.first[0] : nil
+      end
+
       def action_checkout
         if target_dir_non_existent_or_empty?
           clone
@@ -117,40 +319,6 @@ class Chef
 
       def target_dir_non_existent_or_empty?
         !::File.exist?(cwd) || Dir.entries(cwd).sort == ['.', '..']
-      end
-
-      def find_current_revision
-        Chef::Log.info("#{@new_resource} finding current git revision")
-        if ::File.exist?(::File.join(cwd, '.git'))
-          # 128 is returned when we're not in a git repo. this is fine
-          run_opts = { cwd: cwd, returns: [0, 128] }
-          rev_parse_result = git_standard_executor(['rev-parse', 'HEAD'], run_opts)
-          rev = rev_parse_result.stdout.strip
-        end
-        sha_hash?(rev) ? rev : nil
-      end
-
-      def add_remotes
-        unless @new_resource.additional_remotes.empty?
-          @new_resource.additional_remotes.each_pair do |remote_name, remote_url|
-            converge_by("add remote #{remote_name} from #{remote_url}") do
-              Chef::Log.info "#{@new_resource} adding git remote #{remote_name} = #{remote_url}"
-              setup_remote_tracking_branches(remote_name, remote_url)
-            end
-          end
-        end
-      end
-
-      def clone
-        converge_by("clone from #{@new_resource.repository} into #{cwd}") do
-          if @new_resource.uploadpack_allow_reachable_sha1_in_want && (git_minor_version >= Gem::Version.new('2.5.0'))
-            # Introduced in git 2.5 // uploadpack.allowReachableSHA1InWant
-            # https://github.com/git/git/blob/v2.5.0/Documentation/config.txt#L2570
-            clone_by_any_ref
-          else
-            clone_by_advertized_ref
-          end
-        end
       end
 
       def clone_by_any_ref
@@ -198,45 +366,6 @@ class Chef
         args
       end
 
-      def checkout
-        sha_ref = target_revision
-        converge_by("checkout ref #{sha_ref} branch #{@new_resource.revision}") do
-          # checkout into a local branch rather than a detached HEAD
-          run_opts = { cwd: cwd }
-          git_standard_executor(['branch', '-f', @new_resource.checkout_branch, sha_ref], run_opts)
-          git_standard_executor(['checkout', @new_resource.checkout_branch], run_opts)
-          Chef::Log.info "#{@new_resource} checked out branch: #{@new_resource.revision} onto: #{@new_resource.checkout_branch} reference: #{sha_ref}"
-        end
-      end
-
-      # Updated as per https://git-scm.com/book/en/v2/Git-Tools-Submodules
-      # Address https://github.com/chef/chef/issues/4126 (CHEF-4126)
-      def enable_submodules
-        if @new_resource.enable_submodules
-          converge_by("enable git submodules for #{@new_resource}") do
-            Chef::Log.info "#{@new_resource} synchronizing git submodules"
-            run_opts = { cwd: cwd }
-            git_standard_executor(['submodule', 'init'], run_opts)
-
-            Chef::Log.info "#{@new_resource} enabling git submodules"
-            # the --recursive flag means we require git 1.6.5+ now, see CHEF-1827
-            git_standard_executor(['submodule', 'update', '--init', '--recursive'], run_opts)
-          end
-        end
-      end
-
-      def fetch_updates
-        converge_by("fetch updates for #{@new_resource.remote}") do
-          # uploadpack.allowReachableSHA1InWant introduced in git 2.5.0
-          if @new_resource.uploadpack_allow_reachable_sha1_in_want && (git_minor_version >= Gem::Version.new('2.5.0'))
-            fetch_by_any_ref
-          else
-            setup_remote_tracking_branches(@new_resource.remote, @new_resource.repository)
-            fetch_by_advertized_ref
-          end
-        end
-      end
-
       def fetch_by_any_ref
         Chef::Log.info "Fetching [shallow] updates from #{new_resource.remote} and resetting to revision #{target_revision}"
 
@@ -280,138 +409,10 @@ class Chef
         git_standard_executor(git_reset_command, run_opts)
       end
 
-      def setup_remote_tracking_branches(remote_name, remote_url)
-        converge_by("set up remote tracking branches for #{remote_url} at #{remote_name}") do
-          Chef::Log.info "#{@new_resource} configuring remote tracking branches for repository #{remote_url} at remote #{remote_name}"
-          run_opts = { cwd: cwd, returns: [0, 1, 2] }
-          remote_status = git_standard_executor(['config', '--get', "remote.#{remote_name}.url"], run_opts)
-
-          run_opts = { cwd: cwd }
-          case remote_status.exitstatus
-          when 0, 2
-            # * Status 0 means that we already have a remote with this name, so we should update the url
-            #   if it doesn't match the url we want.
-            # * Status 2 means that we have multiple urls assigned to the same remote (not a good idea)
-            #   which we can fix by replacing them all with our target url (hence the --replace-all option)
-
-            if multiple_remotes?(remote_status) || !remote_matches?(remote_url, remote_status)
-              git_standard_executor(['config', '--replace-all', "remote.#{remote_name}.url", remote_url], run_opts)
-            end
-          when 1
-            git_standard_executor(['remote', 'add', remote_name, remote_url], run_opts)
-          end
-        end
-      end
-
-      def multiple_remotes?(check_remote_command_result)
-        check_remote_command_result.exitstatus == 2
-      end
-
-      def remote_matches?(remote_url, check_remote_command_result)
-        check_remote_command_result.stdout.strip.eql?(remote_url)
-      end
-
       def current_revision_matches_target_revision?
         !@current_resource.revision.nil? && (target_revision.strip.to_i(16) == @current_resource.revision.strip.to_i(16))
       end
 
-      def target_revision
-        @target_revision ||= sha_hash?(@new_resource.revision) ? @new_resource.revision : remote_resolve_reference
-      end
-
-      alias revision_slug target_revision
-
-      def remote_resolve_reference
-        Chef::Log.info("#{@new_resource} resolving remote reference")
-        # The sha pointed to by an annotated tag is identified by the
-        # '^{}' suffix appended to the tag. In order to resolve
-        # annotated tags, we have to search for "revision*" and
-        # post-process. Special handling for 'HEAD' to ignore a tag
-        # named 'HEAD'.
-        @resolved_reference = git_ls_remote(rev_search_pattern)
-        refs = @resolved_reference.split("\n").map { |line| line.split("\t") }
-        # First try for ^{} indicating the commit pointed to by an
-        # annotated tag.
-        # It is possible for a user to create a tag named 'HEAD'.
-        # Using such a degenerate annotated tag would be very
-        # confusing. We avoid the issue by disallowing the use of
-        # annotated tags named 'HEAD'.
-
-        found = rev_search_pattern != 'HEAD' ? find_revision(refs, @new_resource.revision, '^{}') : refs_search(refs, 'HEAD')
-        found = find_revision(refs, @new_resource.revision) if found.empty?
-        found.size == 1 ? found.first[0] : nil
-      end
-
-      def find_revision(refs, revision, suffix = '')
-        found = refs_search(refs, rev_match_pattern('refs/tags/', revision) + suffix)
-        found = refs_search(refs, rev_match_pattern('refs/heads/', revision) + suffix) if found.empty?
-        found = refs_search(refs, revision + suffix) if found.empty?
-        found
-      end
-
-      def rev_match_pattern(prefix, revision)
-        revision.start_with?(prefix) ? revision : prefix + revision
-      end
-
-      def rev_search_pattern
-        if ['', 'HEAD'].include? @new_resource.revision
-          'HEAD'
-        else
-          @new_resource.revision + '*'
-        end
-      end
-
-      def git_ls_remote(rev_pattern)
-        stdout_obj = git_standard_executor ['ls-remote', "\"#{@new_resource.repository}\"", "\"#{rev_pattern}\""]
-        stdout_obj.stdout
-      end
-
-      def refs_search(refs, pattern)
-        refs.find_all { |m| m[1] == pattern }
-      end
-
-      private
-
-      def run_options(run_opts = {})
-        env = {}
-        if @new_resource.user
-          run_opts[:user] = @new_resource.user
-          # Certain versions of `git` misbehave if git configuration is
-          # inaccessible in $HOME. We need to ensure $HOME matches the
-          # user who is executing `git` not the user running Chef.
-          env['HOME'] = begin
-            require 'etc'
-            Etc.getpwnam(@new_resource.user).dir
-          rescue ArgumentError # user not found
-            raise Chef::Exceptions::User, "Could not determine HOME for specified user '#{@new_resource.user}' for resource '#{@new_resource.name}'"
-          end
-        end
-        run_opts[:group] = @new_resource.group if @new_resource.group
-        env['GIT_SSH'] = @new_resource.ssh_wrapper if @new_resource.ssh_wrapper
-        run_opts[:log_tag] = @new_resource.to_s
-        run_opts[:timeout] = @new_resource.timeout if @new_resource.timeout
-        env.merge!(@new_resource.environment) if @new_resource.environment
-        run_opts[:environment] = env unless env.empty?
-        run_opts
-      end
-
-      def cwd
-        @new_resource.destination
-      end
-
-      def git(*args)
-        ['git', *args].compact.join(' ')
-      end
-
-      def git_standard_executor(args, run_opts = {})
-        git_command = git(args)
-        Chef::Log.info "> #{git_command}"
-        shell_out!(git_command, run_options(run_opts))
-      end
-
-      def sha_hash?(string)
-        string =~ /^[0-9a-f]{40}$/
-      end
     end
   end
 end
