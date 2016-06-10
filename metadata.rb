bad9feb3416a6c59402aa8131b79191ca13ff8a8
Joshua C. Burt <joshburt@shapeandshare.com>
2016-06-10 13:40:16 -0600

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'joshburt@shapeandshare.com'
 license          'Apache v2.0'
 description      'Installs/Configures jgit'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.1.1'
 issues_url       'https://github.com/joshburt/jgit/issues'
 source_url       'https://github.com/joshburt/jgit'
